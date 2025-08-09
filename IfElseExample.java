61a581600e52656c2701ef3246bd483db502a9fa
yukthayandrapu <2420080060@klh.edu.in>
2025-08-09 09:33:43 +0530

    Add files via upload

---
+++ IfElseExample.java
@@ -0,0 +1,14 @@
+package oops_programs;
+
+public class IfElseExample {
+	public static void main(String[] args) {
+        int number = 5;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
+
