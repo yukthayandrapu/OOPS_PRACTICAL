2ed3e18719a7135af9e4bf5e479b8b4e95efad95
yukthayandrapu <2420080060@klh.edu.in>
2025-08-09 09:31:29 +0530

    Add files via upload

---
+++ IfElselfExample.java
@@ -0,0 +1,19 @@
+package oops_programs;
+
+public class IfElselfExample {
+	 public static void main(String[] args) {
+	        int marks = 75;
+
+	        if (marks >= 90) {
+	            System.out.println("Grade: A+");
+	        } else if (marks >= 75) {
+	            System.out.println("Grade: A");
+	        } else if (marks >= 60) {
+	            System.out.println("Grade: B");
+	        } else {
+	            System.out.println("Grade: C");
+	        }
+	    }
+	}
+
+
