18b0a7f64d96ed08fc31dde6f38fff760660aaf6
yukthayandrapu <2420080060@klh.edu.in>
2025-08-09 09:29:39 +0530

    Add files via upload

---
+++ IfExample.java
@@ -0,0 +1,13 @@
+package oops_programs;
+
+public class IfExample {
+	 public static void main(String[] args) {
+	        int age = 20;
+
+	        if (age >= 18) {
+	            System.out.println("You are eligible to vote.");
+	        }
+	    }
+	}
+
+
