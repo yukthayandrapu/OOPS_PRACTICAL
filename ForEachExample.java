5223612838abb954e9b22bf9f9488173df0e8721
yukthayandrapu <2420080060@klh.edu.in>
2025-08-09 09:35:40 +0530

    Add files via upload

---
+++ ForEachExample.java
@@ -0,0 +1,12 @@
+package oops_programs;
+
+public class ForEachExample {
+	 public static void main(String[] args) {
+	        String[] fruits = {"Apple", "Banana", "Cherry"};
+
+	        for (String fruit : fruits) { // for-each loop
+	            System.out.println(fruit);
+	        }
+	    }
+	}
+
